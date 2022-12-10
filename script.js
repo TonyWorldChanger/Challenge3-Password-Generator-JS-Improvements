a7f2934e66ba7d27f72069bbe9462eb2ac8fd1f4
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2022-12-10 16:44:23 -0600

    added changes to the function generatePassword using iteration and math floor and random

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ var numbers = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
+  var password = "";
+    for(var i = 0; i < characterLength; i++) { // creates loop to randomly select characters using math.floor(math.random) and mutilplying by the length of the array userChoice.
+      var randomLetter = Math.floor(Math.random() * userChoice.length);
+      password = password + userChoice[randomLetter];
+    }
+
+    return password;
+
 
 }
   
@@ -17,22 +25,37 @@ function generateprompts() {
   characterLength = prompt("Length of password? (8 - 128)");
     if(characterLength >= 8 && characterLength <= 128) {
       alert("Character length must be between 8 - 128");
-      return false;
+      return true;
 
     } 
     if(confirm("Would you like lowercase letters in your password?")) {
       userChoice = userChoice + lowercase;
     }
+    if(confirm("Would you like uppercase letters in your password?")) {
+      userChoice = userChoice + uppercase;
+    }
+    if(confirm("Would you like Special Characters in your password?")) {
+      userChoice = userChoice + specialCharacters;
+    }
+    if(confirm("Would you like numbers in your password?")) {
+      userChoice = userChoice +numbers;
+    }
+    return true;
 }
 
 
 
 // Write password to the #password input
-function writePassword() {  
-  var password = generatePassword();
+function writePassword() { 
+  var confirmedPropmts = generateprompts(); 
+    if(confirmedPropmts) {
+      var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+    }
+  
 
 }
 
